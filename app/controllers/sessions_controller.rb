717925500fdcd57ab82a18849e15cbbfcdafcddb
minirachel <rachel.a.levinson@gmail.com>
2021-11-12 12:44:37 -0500

    commit tree 7d33849762c66856900e6ec5de9689b3374907cc

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+    
+    def create
+        @user = User.find_by(name: params[:user][:name])
+        return head(:forbidden) unless @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+    end
+
+    def index
+        @session = session
+    end
+
+end
